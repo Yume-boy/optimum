9976b2bd998987fae0a57db0928a5edd8975d5b8
marvellous <comlaf2001@gmail.com>
2025-10-20 05:23:08 +0100

    done

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,20 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import  crudApi  from "./api/crudApi";
 import authApi from "./api/authApi";
-import { paymentApi } from "./api/paymentApi";
-import { orderApi } from "./api/orderApi";
+
 
 
 export const store = configureStore({
     reducer: {
         [crudApi.reducerPath]: crudApi.reducer,
         [authApi.reducerPath]: authApi.reducer,
-        [paymentApi.reducerPath]: paymentApi.reducer,
-        [orderApi.reducerPath]: orderApi.reducer
     },
 
     middleware: (getDefaultMiddleware) => 
-        getDefaultMiddleware().concat(crudApi.middleware, authApi.middleware, paymentApi.middleware, orderApi.middleware)
+        getDefaultMiddleware().concat(crudApi.middleware, authApi.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>;
